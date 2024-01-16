03928a8b753264598829546f71670498551dc404
Dmytro Domaretskyi <ddmytro787@gmail.com>
2024-01-16 17:39:18 +0200

    Add Prettier, EditorConfig, and Husky for code formatting and conventions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet],
+	template: `
+		<h1>Welcome to {{ title }}!</h1>
 
-    <router-outlet></router-outlet>
-  `,
-  styles: [],
+		<router-outlet></router-outlet>
+	`,
+	styles: [],
 })
 export class AppComponent {
-  title = 'angularlicious';
+	title = 'angularlicious';
 }
