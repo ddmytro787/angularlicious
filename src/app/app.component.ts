60b6a6b2707e19f2339f57feb2941eea1a289e61
Dmytro Domaretskyi <ddmytro787@gmail.com>
2024-01-16 17:55:11 +0200

    Update AppComponent with Material Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,60 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [CommonModule, RouterOutlet],
+	imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule],
 	template: `
-		<h1>Welcome to {{ title }}!</h1>
+		<div class="ags-layout">
+			<mat-toolbar color="primary">
+				<button mat-icon-button (click)="drawer.toggle()">
+					<mat-icon>menu</mat-icon>
+				</button>
+				<span>Angularlicious</span>
+				<span class="spacer"></span>
+			</mat-toolbar>
 
-		<router-outlet></router-outlet>
+			<mat-drawer-container autosize>
+				<mat-drawer #drawer mode="side">Menu</mat-drawer>
+				<div class="ags-page">
+					Page Here
+					<router-outlet></router-outlet>
+				</div>
+			</mat-drawer-container>
+		</div>
 	`,
-	styles: [],
+	styles: [
+		`
+			.ags-layout {
+				display: flex;
+				flex-direction: column;
+				height: 100%;
+				overflow: hidden;
+
+				mat-toolbar {
+					.spacer {
+						flex: 1 1 auto;
+					}
+				}
+
+				mat-drawer-container {
+					flex: 1;
+
+					mat-drawer {
+						min-width: 280px;
+					}
+				}
+
+				.ags-page {
+					padding: 30px;
+				}
+			}
+		`,
+	],
 })
-export class AppComponent {
-	title = 'angularlicious';
-}
+export class AppComponent {}
